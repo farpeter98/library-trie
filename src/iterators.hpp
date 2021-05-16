69cc8591e8f9f1a903cda8b198a0f4decd75db4e
Farkas <farpeter98@gmail.com>
2021-05-16 17:47:24 +0200

    Replaced SFINAE iterator reverseness with constexpr if

--- src/iterators.hpp
+++ src/iterators.hpp
@@ -14,9 +14,6 @@ namespace ltr {
 template<typename N, bool const_v>
 struct consted_type;
 
-template<typename N, bool reverse_v>
-struct reversed_type;
-
 // Bidirectional iterator class
 template<typename N,	// associated node type
 		 bool is_const,
@@ -24,7 +21,6 @@ template<typename N,	// associated node type
 class _Iterator_base {
 private:
 	using node_type  = N;
-	using mover_type = reversed_type<node_type, is_reverse>;
 public:
 	using difference_type   = std::ptrdiff_t;
 	using value_type        = typename consted_type<N, is_const>::val;
@@ -54,7 +50,10 @@ public:
 	}
 
 	_Iterator_base& operator++() noexcept {
-		node = mover_type::increment(node);
+		if constexpr (is_reverse)
+			decrement();
+		else
+			increment();
 		return *this;
 	}
 
@@ -65,7 +64,10 @@ public:
 	}
 
 	_Iterator_base& operator--() noexcept {
-		node = mover_type::decrement(node);
+		if constexpr (is_reverse)
+			increment();
+		else
+			decrement();
 		return *this;
 	}
 
@@ -80,6 +82,91 @@ public:
 	}
 
 private:
+	// preorder-like tree traversal for next element containing a value
+	// from begin to end returns a lexicographically ordered sequence of keys
+	// some invariants: -node always either has value, or is the root
+	//                  -leaf nodes always have value, hence no check when descending to a leaf
+	void increment() noexcept {
+		// case 1: current node is not a leaf
+		if (node->child) {
+			node = node->child;
+			// traverse along left children until found a node with a value
+			// although not explicitly checked, if a node has no value
+			// it can't be a leaf, thus it always has a child
+			while (!(node->value.has_value()))
+				node = node->child;
+			return;
+		}
+
+		// case 2: current node has a right-side sibling
+		if (node->next) {
+			node = node->next;
+			// traverse along left children until found a node with a value
+			while (!(node->value.has_value()))
+				node = node->child;
+			return;
+		}
+
+		// case 3: current node has no children or right-side sibling
+		// need to ascend until there's a right-side sibling, or we're at the root
+		while (node->next == nullptr && node->parent)
+			node = node->parent;
+		// same as case 2
+		if (node->next) {
+			node = node->next;
+			while (!(node->value.has_value()))
+				node = node->child;
+		}
+		// node is either root or a node in a rightside subtree
+	}
+
+	void decrement() noexcept {
+		// case 1: node has a left-side sibling - either leaf or intermediate node
+		if (node->prev) {
+			node = node->prev;
+			// go to its rightmost child until on a leaf
+			while (node->child) {
+				node = node->child;
+				while (node->next)
+					node = node->next;
+			}
+			return;
+		}
+
+		// case 2: no left-side sibling
+		// ascend until found a node with a value, or a left-side sibling
+		if (node->parent) {
+			node = node->parent;
+			while (!(node->value.has_value()) && node->prev == nullptr && node->parent)
+				node = node->parent;
+
+			// if found a value, return
+			if (node->value.has_value())
+				return;
+
+			// if there's a left-side sibling, repeat case 1
+			if (node->prev) {
+				node = node->prev;
+				while (node->child) {
+					node = node->child;
+					while (node->next)
+						node = node->next;
+				}
+			}
+
+			// reached left-subtree target node or the root
+			return;
+		}
+
+		// case 3: started at the root
+		// descend to the rightmost child
+		while (node->child) {
+			node = node->child;
+			while (node->next)
+				node = node->next;
+		}
+	}
+
 	node_type* node;
 }; // class _Iterator_base
 
@@ -96,108 +183,6 @@ struct consted_type<N, false> {
 	using ptr = val*;
 	using ref = val&;
 };
-
-template<typename N>
-struct reversed_type<N, false> {
-
-	// preorder-like tree traversal for next element containing a value
-	// from begin to end returns a lexicographically ordered sequence of keys
-	// some invariants: -node always either has value, or is the root
-	//                  -leaf nodes always have value, hence no check when descending to a leaf
-	static N* increment(N* node) noexcept {
-		// case 1: current node is not a leaf
-		if (node->child) {
-			node = node->child;
-			// traverse along left children until found a node with a value
-			// although not explicitly checked, if a node has no value
-			// it can't be a leaf, thus it always has a child
-			while (!(node->value.has_value()))
-				node = node->child;
-			return node;
-		}
-
-		// case 2: current node has a right-side sibling
-		if (node->next) {
-			node = node->next;
-			// traverse along left children until found a node with a value
-			while (!(node->value.has_value()))
-				node = node->child;
-			return node;
-		}
-
-		// case 3: current node has no children or right-side sibling
-		// need to ascend until there's a right-side sibling, or we're at the root
-		while (node->next == nullptr && node->parent)
-			node = node->parent;
-		// same as case 2
-		if (node->next) {
-			node = node->next;
-			while (!(node->value.has_value()))
-				node = node->child;
-		}
-		// node is either root or a node in a rightside subtree
-		return node;
-	}
-
-	static N* decrement(N* node) noexcept {
-		// case 1: node has a left-side sibling - either leaf or intermediate node
-		if (node->prev) {
-			node = node->prev;
-			// go to its rightmost child until on a leaf
-			while (node->child) {
-				node = node->child;
-				while (node->next)
-					node = node->next;
-			}
-			return node;
-		}
-
-		// case 2: no left-side sibling
-		// ascend until found a node with a value, or a left-side sibling
-		if (node->parent) {
-			node = node->parent;
-			while (!(node->value.has_value()) && node->prev == nullptr && node->parent)
-				node = node->parent;
-
-			// if found a value, return
-			if (node->value.has_value())
-				return node;
-
-			// if there's a left-side sibling, repeat case 1
-			if (node->prev) {
-				node = node->prev;
-				while (node->child) {
-					node = node->child;
-					while (node->next)
-						node = node->next;
-				}
-			}
-
-			// reached left-subtree target node or the root
-			return node;
-		}
-
-		// case 3: started at the root
-		// descend to the rightmost child
-		while (node->child) {
-			node = node->child;
-			while (node->next)
-				node = node->next;
-		}
-		return node;
-	}
-}; // struct reverseness_struct
-
-template<typename N>
-struct reversed_type<N, true> {
-	static N* increment(N* node) {
-		return reversed_type<N, false>::decrement(node);
-	}
-
-	static N* decrement(N* node) {
-		return reversed_type<N, false>::increment(node);
-	}
-}; // struct reverseness_struct
 
 } // namespace ltr
 
